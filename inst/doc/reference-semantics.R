eaf46490471e50da4e1b51506950baa7a1b7868d
Roman Pahl <roman.pahl@gmail.com>
2022-02-19 14:30:02 +0000

    version 1.0.1

--- inst/doc/reference-semantics.R
+++ inst/doc/reference-semantics.R
@@ -2,9 +2,36 @@
 require(container)
 knitr::opts_chunk$set(
   comment = "#",
-    error = FALSE,
-     tidy = FALSE,
-    cache = FALSE,
- collapse = TRUE
+  prompt = F,
+  tidy = FALSE,
+  cache = FALSE,
+  collapse = T
 )
+
+options(width = 100L)
+
+## -------------------------------------------------------------------------------------------------
+co = container()
+
+ref_add(co, a = 1)
+ref_add(co, b = 2)
+
+co
+
+## ---- eval = FALSE--------------------------------------------------------------------------------
+#  co = container() |>
+#      add(a = 1)   |>
+#      add(b = 2)
+
+## -------------------------------------------------------------------------------------------------
+odds = container()
+evens = container()
+isOdd = function(x) {x %% 2 == 1}
+
+res = sapply(1:10, function(i) 
+    if(isOdd(i)) ref_add(odds, i) else ref_add(evens, i)
+)
+
+odds
+evens
 
